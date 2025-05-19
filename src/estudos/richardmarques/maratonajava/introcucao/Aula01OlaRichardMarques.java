2a10d9c7c1d70d69f6cd828651a4d73046a6aea7
rrichard-marques <rri_11@hotmail.com>
2025-05-19 15:05:31 -0300

    :books: Commit Inicial

---
+++ src/estudos/richardmarques/maratonajava/introcucao/Aula01OlaRichardMarques.java
@@ -0,0 +1,9 @@
+package estudos.richardmarques.maratonajava.introcucao;
+
+public class Aula01OlaRichardMarques {
+    public static void main(String[] args) {
+
+        System.out.println ("KA-ME-HA-ME-HAAA");
+
+    }
+}
---
+++ src/estudos/richardmarques/maratonajava/introcucao/Aula02TiposPrimitivos.java
@@ -0,0 +1,24 @@
+package estudos.richardmarques.maratonajava.introcucao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+        int idade = 10;
+        long numeroGrande = (long) 155.23D;
+        double salarioDouble = 2000;
+        float salarioFloat = (float) 2500.0D;
+        byte idadeByte = 120;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 10;
+        String nome = "Goku";
+
+        System.out.println ("A idade de João é " + idade + " anos");
+        System.out.println (false);
+        System.out.println (salarioFloat);
+        System.out.println (numeroGrande);
+        System.out.println ("Oi, meu nome é "+nome);
+
+    }
+}
