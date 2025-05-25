8cc6ccab554db3cb7409224fad8b84b9804f2ca6
rrichard-marques <rri_11@hotmail.com>
2025-05-25 15:24:19 -0300

    Create Aula06EstruturasDeRepeticao01.java

---
+++ src/introducao/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,11 @@
+package introducao;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+
+        for (int i = 0 ; i <= 10 ; i++ ) {
+            System.out.println("For " + i);
+        }
+
+    }
+}
