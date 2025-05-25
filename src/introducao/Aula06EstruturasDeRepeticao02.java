4874c04dc4666a2537c7a1c2f7f33a7e8d9be221
rrichard-marques <rri_11@hotmail.com>
2025-05-25 15:25:21 -0300

    Create Aula06EstruturasDeRepeticao02.java

---
+++ src/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+
+        for (int i = 0 ; i < 50 ; i++) {
+            if (i <= 25) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
