e96b81ad77ce8acf1fdcaa2de83c48fe5a0bc1a8
rrichard-marques <rri_11@hotmail.com>
2025-05-27 15:08:12 -0300

    Create Aula07Arrays01.java

---
+++ src/introducao/Aula07Arrays01.java
@@ -0,0 +1,18 @@
+package introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+
+        int [] idades = new int[5];
+        idades [0] = 10;
+        idades [1] = 11;
+        idades [2] = 12;
+        idades [3] = 13;
+        idades [4] = 14;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[3]);
+        System.out.println(idades[4]);
+
+    }
+}
