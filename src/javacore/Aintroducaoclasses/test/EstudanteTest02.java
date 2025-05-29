5f5543a7b3d8dd27d048a5f392ff7b672911f1d0
rrichard-marques <rri_11@hotmail.com>
2025-05-29 13:29:54 -0300

    Create EstudanteTest02.java

---
+++ src/javacore/Aintroducaoclasses/test/EstudanteTest02.java
@@ -0,0 +1,12 @@
+package javacore.Aintroducaoclasses.test;
+
+import javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudadente = new Estudante();
+        estudadente.nome = "Richard";
+        estudadente.idade = 25;
+        estudadente.sexo = 'M';
+    }
+}
