8336438327dfdf4a1fe1708bf8e9427ddcac6ef2
rrichard-marques <rri_11@hotmail.com>
2025-06-02 13:18:37 -0300

    Create CalculadoraTest01.java

---
+++ src/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,19 @@
+package javacore.Bintroducaometodos.test;
+
+import estudos.richardmarques.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+
+
+
+        /*Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();*/
+
+        Calculadora calculadora = new Calculadora();
+        calculadora.subtraiDoisNumeros();
+
+
+    }
+}
