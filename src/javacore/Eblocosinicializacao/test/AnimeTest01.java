ebf638a50ce04a235c1bf077570e909121c88dfb
rrichard-marques <rri_11@hotmail.com>
2025-06-10 09:35:50 -0300

    Create AnimeTest01.java

---
+++ src/javacore/Eblocosinicializacao/test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package javacore.Eblocosinicializacao.test;
+
+import javacore.Eblocosinicializacao.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+        //Anime anime1 = new Anime();
+
+        Anime eposodios = new Anime();
+
+    }
+}
