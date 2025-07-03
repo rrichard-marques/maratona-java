2361ecf938a11fbd4cda9135ad043ac338119087
rrichard-marques <rri_11@hotmail.com>
2025-07-03 16:55:37 -0300

    Create ClienteTest01.java

---
+++ src/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,19 @@
+package javacore.Kenum.test;
+
+import javacore.Kenum.dominio.Cliente;
+import javacore.Kenum.dominio.TipoCliente;
+import javacore.Kenum.dominio.TipoPagamento;
+
+public class ClienteTest01 {
+    public static void main(String[] args) {
+
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(40) + "%");
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(40) + "%");
+
+    }
+}
