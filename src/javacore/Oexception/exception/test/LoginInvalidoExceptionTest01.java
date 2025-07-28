5261e26b8a54c80cd70627f1eeb48544fcd775a3
rrichard-marques <rri_11@hotmail.com>
2025-07-28 14:03:31 -0300

    Create LoginInvalidoExceptionTest01.java

---
+++ src/javacore/Oexception/exception/test/LoginInvalidoExceptionTest01.java
@@ -0,0 +1,34 @@
+package javacore.Oexception.exception.test;
+
+import javacore.Oexception.exception.dominio.LoginInvalidoException;
+
+import java.util.Scanner;
+
+public class LoginInvalidoExceptionTest01 {
+    public static void main(String[] args) {
+
+        try {
+            logar();
+        } catch (LoginInvalidoException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void logar() throws LoginInvalidoException {
+
+        Scanner input = new Scanner(System.in);
+        String usuarioDB = "Goku";
+        String senhaDB = "ssj";
+        System.out.println("Usuário:");
+        String usuarioDigitado = input.nextLine();
+        System.out.println("Usuário:");
+        String senhaDigitada = input.nextLine();
+        if (!usuarioDigitado.equals(usuarioDB) || !senhaDigitada.equals(senhaDB)) {
+            throw new LoginInvalidoException("Usuário ou senha inválidos.");
+        }
+        System.out.println("Login feito com sucesso!");
+
+    }
+
+}
