6ff4dae5d9e34c2ae26fc27156217ec0124c6e36
rrichard-marques <rri_11@hotmail.com>
2025-09-02 13:01:25 -0300

    Create NumberFormatTest02.java

---
+++ src/javacore/Sformatacao/test/NumberFormatTest02.java
@@ -0,0 +1,27 @@
+package javacore.Sformatacao.test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatTest02 {
+    public static void main(String[] args) {
+
+        Locale locale = Locale.getDefault();
+        Locale localeBR = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+
+        NumberFormat[] nfa = new NumberFormat[4];
+        nfa[0] = NumberFormat.getCurrencyInstance(locale);
+        nfa[1] = NumberFormat.getCurrencyInstance(localeBR);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
+        double valor = 1_000_000.000;
+
+        for (NumberFormat numberFormat : nfa) {
+            System.out.println(numberFormat.format(valor));
+        }
+
+
+    }
+}
