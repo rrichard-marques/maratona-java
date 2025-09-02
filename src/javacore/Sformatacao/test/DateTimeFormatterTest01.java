e616a69b1e1090e42de9b61b54dd8d36baa66f7c
rrichard-marques <rri_11@hotmail.com>
2025-09-02 13:00:21 -0300

    Create DateTimeFormatterTest01.java

---
+++ src/javacore/Sformatacao/test/DateTimeFormatterTest01.java
@@ -0,0 +1,33 @@
+package javacore.Sformatacao.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatterTest01 {
+    public static void main(String[] args) {
+
+        LocalDate date = LocalDate.now();
+        String s1 = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String s2 = date.format(DateTimeFormatter.ISO_DATE);
+        String s3 = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        LocalDate parse1 = LocalDate.parse("20250513", DateTimeFormatter.BASIC_ISO_DATE);
+        System.out.println(parse1);
+
+        LocalDateTime now = LocalDateTime.now();
+        String s4 = now.format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(s4);
+        LocalDateTime parse4 = LocalDateTime.parse("2025-05-13T20:04:25.2684879", DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(parse4);
+
+        DateTimeFormatter fomart1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formatBR = LocalDate.now().format(fomart1);
+        System.out.println(formatBR);
+
+    }
+}
